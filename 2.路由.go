ab7ffb72da31e7a93f8b2453cc7a9e3181c35ec6
fengfeng <fengfengzhidao@126.com>
2024-06-02 01:03:29 +0800

    中间件

--- 2.路由.go
+++ 2.路由.go
@@ -1,2 +1,36 @@
 // /2.路由.go
 package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func UserView(c *gin.Context) {
+	path := c.Request.URL
+	fmt.Println(c.Request.Method, path)
+}
+
+func main() {
+	r := gin.Default()
+
+	apiGroup := r.Group("api")
+	apiGroup.Use()
+	UserGroup(apiGroup)
+
+	noMiddleWareGroup := r.Group("api")
+	LoginGroup(noMiddleWareGroup)
+
+	r.Run(":8080")
+}
+
+func UserGroup(r *gin.RouterGroup) {
+	r.GET("users", UserView)
+	r.POST("users", UserView)
+	r.DELETE("users", UserView)
+	r.PUT("users", UserView)
+}
+
+func LoginGroup(r *gin.RouterGroup) {
+	r.GET("login", UserView)
+}
--- 3.中间件.go
+++ 3.中间件.go
@@ -1,2 +1,65 @@
 // /3.中间件.go
 package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func Home(c *gin.Context) {
+	fmt.Println("Home")
+	fmt.Println(c.Get("GM1"))
+	fmt.Println(c.Get("GM2"))
+	_user, ok := c.Get("user")
+	if ok {
+		user, ok := _user.(UserInfo)
+		if ok {
+			fmt.Println(user.Name)
+			panic("xxx")
+		}
+	}
+	c.String(200, "Home")
+}
+func M1(c *gin.Context) {
+	fmt.Println("M1 请求部分")
+	c.Abort()
+	fmt.Println("M1 响应部分")
+}
+func M2(c *gin.Context) {
+	fmt.Println("M2 请求部分")
+	c.Next()
+	fmt.Println("M2 响应部分")
+}
+
+type UserInfo struct {
+	Name string
+}
+
+func GM1(c *gin.Context) {
+	fmt.Println("GM1 请求部分")
+	var user = UserInfo{Name: "枫枫"}
+	c.Set("GM1", "GM1")
+	c.Set("user", user)
+	c.Next()
+	fmt.Println("GM1 响应部分")
+}
+
+func GM2(c *gin.Context) {
+	fmt.Println("GM2 请求部分")
+	c.Set("GM2", "GM2")
+	c.Next()
+	fmt.Println("GM2 响应部分")
+	fmt.Println(c.Get("GM2"))
+}
+
+func AuthMiddleware(c *gin.Context) {
+
+}
+
+func main() {
+	r := gin.Default()
+	g := r.Group("api")
+	g.Use(GM1, GM2)
+	g.GET("users", Home)
+	r.Run(":8080")
+}
